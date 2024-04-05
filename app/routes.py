7b36efa8d741397abde1e2dc1e03f012001518de
Mustafa Bolat <mustafabolat6@gmail.com>
2024-04-05 16:35:26 -0400

    working on db and overall project structure

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+# > flask --debug --app app.views run
+
+from app import app
+from flask import Flask, render_template
+from models import User, Event
+
+
+def register_routes(app, db):
+
+    @app.route('/testdb')
+    def testdb():
+        events = Event.query.all()
+        return str(events)
+
+    #SPLASHPAGE
+    @app.route('/')
+    def hello():
+        return render_template('index.html')
+
+    #LOGIN
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    #REGISTER
+    @app.route('/register')
+    def register():
+        return 'register page'
+
+    #HOME PAGE
+    @app.route('/home')
+    def home():
+        return render_template('home.html')
+
+    @app.route('/myevents')
+    def myevents():
+        return render_template('myevents.html')
+
